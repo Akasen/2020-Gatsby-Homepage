21b422dce9e31ccf8558b5ff0037299e87fa8b3f
Joshua Faria <Akasen1226@gmail.com>
2020-03-29 23:49:43 -0400

    add in Main component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,19 @@
 import React from "react"
 
-import Layout from "./components/layout"
+import Layout from "./components/Layout"
+import Main from "./components/Main"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>A MISSING PAGE!?</h1>
-    <p>
-      You've either tried to manually enter a page that doesn't exist, clicked a
-      link to something deleted, or I forgot to update something
-    </p>
+    <Main>
+      <div className="showcase">
+        <h1>A MISSING PAGE!?</h1>
+        <p>
+          You've either tried to manually enter a page that doesn't exist,
+          clicked a link to something deleted, or I forgot to update something
+        </p>
+      </div>
+    </Main>
   </Layout>
 )
 
