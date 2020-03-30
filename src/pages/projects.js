01dc1353cba640bd0e3306b35159d870f32cbd92
Joshua Faria <Akasen1226@gmail.com>
2020-03-30 00:14:10 -0400

    add text regarding "This Website" and more

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,32 +1,34 @@
 import React from "react"
 import Layout from "./components/Layout"
+import { Link } from "gatsby"
 
 const Projects = () => (
   <div>
     <Layout>
       <div class="showcase">
-        <h1>Projects Page</h1>
+        <h1>
+          <a
+            href="https://github.com/Akasen/2020-Gatsby-Homepage"
+            target="noopener noreferrer"
+          >
+            This Website
+          </a>
+        </h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta
-          minus molestiae vel beatae natus eveniet ratione temporibus aperiam
-          harum alias officiis assumenda officia quibusdam deleniti eos
-          cupiditate dolore doloribus!
+          This homepage has been quite the learning experience so far. I've
+          remade it many times over the years, but this current iteration I'm
+          taking more active steps in learning some tools.
         </p>
         <p>
-          Ad dolore dignissimos asperiores dicta facere optio quod commodi nam
-          tempore recusandae. Rerum sed nulla eum vero expedita ex delectus
-          voluptates rem at neque quos facere sequi unde optio aliquam!
+          This iteration was originally just made in basic HTML and CSS, but
+          then I realized it was a perfect opportunity to take what I had there
+          and use them as a way to guide me in the creation of a basic Gatsby
+          website.
         </p>
         <p>
-          Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur
-          porro quaerat autem ipsam odit quam beatae tempora quibusdam illum!
-          Modi velit odio nam nulla unde amet odit pariatur at!
-        </p>
-        <p>
-          Consequatur rerum amet fuga expedita sunt et tempora saepe? Iusto
-          nihil explicabo perferendis quos provident delectus ducimus
-          necessitatibus reiciendis optio tempora unde earum doloremque commodi
-          laudantium ad nulla vel odio?
+          I'm so far happy with the results so far, and have in a short time
+          learned a great deal about Gatsby and gained a better basic
+          understanding of ReactJS.
         </p>
       </div>
 
