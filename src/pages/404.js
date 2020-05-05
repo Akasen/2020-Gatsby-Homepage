162d7d5b281cd758617a6e545c20fda7c326e5f8
Joshua Faria <Akasen1226@gmail.com>
2020-05-05 09:52:38 -0400

    update page to at least say its a 404. Add text

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,10 +7,14 @@ const NotFoundPage = () => (
   <Layout>
     <Main>
       <div className="showcase">
-        <h1>A MISSING PAGE!?</h1>
+        <h1>404 -A MISSING PAGE!?</h1>
         <p>
           You've either tried to manually enter a page that doesn't exist,
           clicked a link to something deleted, or I forgot to update something
+        </p>
+        <p>
+          I also want to write a funny quip about "If you're not seeing this
+          page but instead a generic 404 message", but that's dumb
         </p>
       </div>
     </Main>
