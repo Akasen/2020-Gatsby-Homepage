1972b4f3401f7535de7d71df3712e760a7e0839a
Akasen <akasen@protonmail.com>
2020-08-25 20:52:21 -0400

    update packages

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "./components/Layout"
+
+const Contact = () => (
+  <div>
+    <Layout>
+      <div>If you would like to contact me</div>
+    </Layout>
+  </div>
+)
+
+export default Contact
